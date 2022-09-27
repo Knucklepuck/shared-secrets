3c58d599464f91031ebfaf06bd87455b903bcd97
Nathan Stryker <nstryker@gmail.com>
2022-09-26 19:05:53 -0700

    Hide password option.

--- pages/read/post-decrypt.php
+++ pages/read/post-decrypt.php
@@ -1,8 +1,7 @@
 <?php
+
   # prevent direct access
-  if (!defined("SYS11_SECRETS")) {
-      die("");
-  }
+  if (!defined("SYS11_SECRETS")) { die(""); }
 
   # define page title
   define("PAGE_TITLE", "Read a Secret.");
@@ -11,39 +10,46 @@
 
   # set the correct response code on error
   if ((null === $secret) || (false !== $error)) {
-      http_response_code(403);
+    http_response_code(403);
   }
 
   if (PLAIN_PARAM) {
-      # set correct content type
-      header("Content-Type: text/plain");
+    # set correct content type
+    header("Content-Type: text/plain");
 
-      if ((null !== $secret) && (false === $error)) {
-          print($secret);
-      }
+    if ((null !== $secret) && (false === $error)) {
+      print($secret);
+    }
   } else {
-      if ((null !== $secret) && (false === $error)) {
-          $secret = html($secret);
-      } else {
-          $secret = "<strong>ERROR:</strong> ".html($error);
-      }
+    if ((null !== $secret) && (false === $error)) {
+      $secret = html($secret);
+    } else {
+      $secret = "<strong>ERROR:</strong> ".html($error);
+    }
 
-      # include header
-      require_once(ROOT_DIR."/template/header.php");
+    # include header
+    require_once(ROOT_DIR."/template/header.php");
 
-      # prevents cache hits with wrong CSS
-      $cache_value = md5_file(__FILE__);
+    # prevents cache hits with wrong CSS
+    $cache_value = md5_file(__FILE__);
 
-      ?>
+?>
 
   <noscript>
     <div class="alert alert-warning">
       <strong>Warning!</strong> You don't have JavaScript enabled. You will not be able to read password-protected secrets.
     </div>
   </noscript>
+  <div class="alert alert-danger" id="decrypt-error">
+    <strong>Error!</strong> Local decryption failed.
+  </div>
 
   <h1>Read a Secret:</h1>
   <p><pre id="secret"><?= $secret ?></pre></p>
+
+  <label class="checkbox-inline" for="decrypt-locally"><input type="checkbox" autocomplete="off" id="decrypt-locally" value="" />Password-protected: </label>
+  <input type="password" autocomplete="off" class="form-control" id="password" maxlength="64" size="32" />
+  <input type="button" class="btn btn-default" id="decrypt" value="Unprotect!" />
 
   <link href="/resources/css/read.css?<?= $cache_value ?>" integrity="sha256-wgpxEGDMqG2EJxicZqc40OJMPwN8rBAZTYLdGyagQGw=" rel="stylesheet" type="text/css" />
 
@@ -53,6 +59,7 @@
 
 <?php
 
-          # include footer
-          require_once(ROOT_DIR."/template/footer.php");
+    # include footer
+    require_once(ROOT_DIR."/template/footer.php");
   }
+
--- pages/share/get-encrypt.php
+++ pages/share/get-encrypt.php
@@ -1,8 +1,7 @@
 <?php
+
   # prevent direct access
-  if (!defined("SYS11_SECRETS")) {
-      die("");
-  }
+  if (!defined("SYS11_SECRETS")) { die(""); }
 
   # define page title
   define("PAGE_TITLE", "Share a Secret.");
@@ -13,13 +12,16 @@
   # prevents cache hits with wrong CSS
   $cache_value = md5_file(__FILE__);
 
-  ?>
+?>
 
   <noscript>
     <div class="alert alert-warning">
       <strong>Warning!</strong> You don't have JavaScript enabled. You will not be able to share password-protected secrets.
     </div>
   </noscript>
+  <div class="alert alert-danger" id="encrypt-error">
+    <strong>Error!</strong> Local encryption failed.
+  </div>
 
   <form role="form" action="/<?= html(SECRET_URI) ?><?= (PLAIN_PARAM) ? "?plain" : "" ?>" method="post">
     <h1>Share a Secret:</h1>
@@ -30,23 +32,28 @@
     <button type="submit" class="btn btn-default pull-right" id="share-secret-btn" name="share-secret-btn">Share the Secret!</button>
   </form>
 
+  <label class="checkbox-inline" for="encrypt-locally"><input type="checkbox" autocomplete="off" id="encrypt-locally" value="" />Password-protected: </label>
+  <input type="password" autocomplete="off" class="form-control" id="password" maxlength="64" size="32" />
+  <input type="button" class="btn btn-default" id="encrypt" value="Protect!" />
+
   <link href="/resources/css/share.css?<?= $cache_value ?>" integrity="sha256-EYu1Dc10IDi0yUOyV55YWmCKWfVlBj1rTMk/AsbViKE=" rel="stylesheet" type="text/css" />
 
   <script src="/resources/js/lib.js?<?= $cache_value ?>" integrity="sha256-TSNgGTWMqT8DICfF7UgTtxjnc/G935Ml4oxIQnHAxSM=" type="text/javascript"></script>
 <?php
-    if (defined("JUMBO_SECRETS") && JUMBO_SECRETS) {
-        ?>
+  if (defined("JUMBO_SECRETS") && JUMBO_SECRETS) {
+?>
   <script src="/resources/js/jumbo_limit.js?<?= $cache_value ?>" integrity="sha256-7OnyT9osWKeiIPJ7xJ8IF1UYF3c/rpy2+ku0sQ0oue4=" type="text/javascript"></script>
 <?php
-    } else {
-        ?>
+  } else {
+?>
   <script src="/resources/js/limit.js?<?= $cache_value ?>" integrity="sha256-HwcYaoqBBJhR7Y7eG2CepXkamos6C6SaViLGifuuo4E=" type="text/javascript"></script>
 <?php
-    }
-  ?>
+  }
+?>
   <script src="/resources/js/share.js?<?= $cache_value ?>" integrity="sha256-JgwhPbFEIzq89yXPJxa5NkZsH8F5MtkCsQ/5sHwU+gg=" type="text/javascript"></script>
 
 <?php
 
-    # include footer
-    require_once(ROOT_DIR."/template/footer.php");
+  # include footer
+  require_once(ROOT_DIR."/template/footer.php");
+
