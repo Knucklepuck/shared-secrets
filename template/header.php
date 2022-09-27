d8d1d64947731614c5d73d482279f919663e8ca0
Nathan Stryker <nstryker@gmail.com>
2022-09-26 18:50:20 -0700

    Minimize header.

--- template/header.php
+++ template/header.php
@@ -1,12 +1,13 @@
 <?php
-
   # prevent direct access
-  if (!defined("SYS11_SECRETS")) { die(""); }
+  if (!defined("SYS11_SECRETS")) {
+      die("");
+  }
 
   # prevents cache hits with wrong CSS
   $cache_value = md5_file(__FILE__);
 
-?>
+  ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -25,33 +26,9 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/"><?php print(htmlentities(SERVICE_TITLE)); ?></a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li<?php if (empty(SECRET_URI)) { ?> class="active"<?php } ?>><a href="/">Share a secret.</a></li>
-            <li<?php if (0 === strcmp(SECRET_URI, HOW_PAGE_NAME)) { ?> class="active"<?php } ?>><a href="/how">How does this service work?</a></li>
-            <li<?php if (0 === strcmp(SECRET_URI, PUB_PAGE_NAME)) { ?> class="active"<?php } ?>><a href="/pub">Download the public key.</a></li>
-            <li<?php if (0 === strcmp(SECRET_URI, IMPRINT_PAGE_NAME)) { ?> class="active"<?php } ?>><a href="/imprint"><?= (defined("IMPRINT_TEXT") && (null !== IMPRINT_TEXT)) ? html(IMPRINT_TEXT) : "Who provides this service?" ?></a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
     <div class="jumbotron text-center">
-      <h1><?php print(htmlentities(PAGE_TITLE)); ?></h1>
-      <p>This page allows you to share a secret through a secret sharing link.<br />
-         The secret is stored in the secret sharing link and not on the server.<br />
-         A secret sharing link can only be used once.</p>
+      <h1><?php print(htmlentities(SERVICE_TITLE)); ?></h1>
+      <p>Paste a password, secret message or private link below.</p>
     </div>
 
     <div class="container">
